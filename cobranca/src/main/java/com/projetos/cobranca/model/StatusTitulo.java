907e23e177faa0b266d2463b191c95fb9f3b7d02
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2020-03-25 11:20:49 -0300

    initial

---
+++ cobranca/src/main/java/com/projetos/cobranca/model/StatusTitulo.java
@@ -0,0 +1,16 @@
+package com.projetos.cobranca.model;
+
+public enum StatusTitulo {
+	PENDENTE("Pendente"),
+	RECEBIDO("Recebido");
+	
+	private String descricao;
+
+	StatusTitulo(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+}
---
+++ cobranca/src/main/java/com/projetos/cobranca/repository/Titulos.java
@@ -0,0 +1,9 @@
+package com.projetos.cobranca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetos.cobranca.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long>{
+
+}
